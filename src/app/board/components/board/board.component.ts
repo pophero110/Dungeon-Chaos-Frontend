d0a8fc6101e1d656bdba19a375a970cee2a37293
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-02 14:08:24 -0500

    feat(board): display player token after player creation

--- src/app/board/components/board/board.component.ts
+++ src/app/board/components/board/board.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectBoard } from '../../state/board.selectors';
 import { fetchBoard } from '../../state/board.actions';
+import { selectPlayer } from 'src/app/player/state/player.selectors';
 
 @Component({
   selector: 'app-board',
@@ -9,6 +10,7 @@ import { fetchBoard } from '../../state/board.actions';
   styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
+  player = this.store.select(selectPlayer);
   board = this.store.select(selectBoard);
   constructor(private store: Store) {}
   ngOnInit(): void {
