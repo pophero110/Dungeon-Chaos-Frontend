4ebef257f8ac93e1b0f3e2cbe66ba10016be7411
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-02 08:17:23 -0500

    refactor(board): create board service folder to organize service file

--- src/app/service/board/board.service.ts
+++ src/app/service/board/board.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { fetchBoardUrl } from '../utils/boardApi';
+import { fetchBoardUrl } from '../../utils/boardApi';
 @Injectable({
   providedIn: 'root',
 })
