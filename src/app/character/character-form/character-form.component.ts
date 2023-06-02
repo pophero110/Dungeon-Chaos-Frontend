61a1ac17fd81e906a51413e5ec8aa1007abcdd6d
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-01 21:20:34 -0500

    feat(character): create character form component

---
+++ src/app/character/character-form/character-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-character-form',
+  templateUrl: './character-form.component.html',
+  styleUrls: ['./character-form.component.scss'],
+})
+export class CharacterFormComponent {}
