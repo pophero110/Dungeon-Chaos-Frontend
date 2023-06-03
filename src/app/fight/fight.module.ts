f5e59d6cb5bf01849df773e6e7298fa5867f4fb3
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-03 17:50:07 -0500

    feat(fight): create fight module

---
+++ src/app/fight/fight.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { TokenModule } from '../token/token.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, TokenModule],
+  exports: [],
+})
+export class CharacterModule {}
