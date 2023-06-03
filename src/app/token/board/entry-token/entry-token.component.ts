5943e913d618969874738d015eefb961d564bc5d
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-02 20:32:52 -0500

    feat(token): add portal and entry token

---
+++ src/app/token/board/entry-token/entry-token.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-token',
+  templateUrl: './entry-token.component.html',
+  styleUrls: ['./entry-token.component.scss']
+})
+export class EntryTokenComponent {
+
+}
