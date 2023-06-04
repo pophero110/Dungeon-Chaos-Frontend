cdf2faa14dc241296db834043c6ba7d4634fccfe
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-03 19:43:45 -0500

    feat(player): display player panel after player creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectPlayer } from './player/state/player.selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'dungeon-chaos-frontend';
+
+  player$ = this.store.select(selectPlayer);
+  constructor(private store: Store) {}
 }
