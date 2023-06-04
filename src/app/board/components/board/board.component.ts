d52d4e1e4e692c6b787246ece52cf58d34b9352c
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-03 19:02:02 -0500

    refactor(board): refactor board component

--- src/app/board/components/board/board.component.ts
+++ src/app/board/components/board/board.component.ts
@@ -9,54 +9,58 @@ import {
   selectPlayer,
   selectPlayerCharacterName,
 } from 'src/app/player/state/player.selectors';
-import { distinctUntilChanged, first, map } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs';
 import { startFight } from 'src/app/fight/state/fight.actions';
 import { isValidMove } from '../../board.utils';
+import { Player } from 'src/app/player/player.model';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
-  player = this.store.select(selectPlayer);
-  board = this.store.select(selectBoard);
-  currentPlayerPosition = this.store
+  player$ = this.store.select(selectPlayer);
+  board$ = this.store.select(selectBoard);
+  currentPlayerPosition$ = this.store
     .select(selectCurrentPlayerPosition)
     .pipe(distinctUntilChanged());
-  playerCharacterName = this.store
+  playerCharacterName$ = this.store
     .select(selectPlayerCharacterName)
     .pipe(distinctUntilChanged());
 
-  constructor(private store: Store) {}
+  currentPlayerPosition!: number | null;
+  playerCharacterName: string | undefined;
+  player!: Player | null;
+  constructor(private store: Store) {
+    this.currentPlayerPosition$
+      .pipe(map((position) => (this.currentPlayerPosition = position)))
+      .subscribe();
+
+    this.playerCharacterName$
+      .pipe(map((name) => (this.playerCharacterName = name)))
+      .subscribe();
+
+    this.player$.subscribe((player) => {
+      this.player = player;
+    });
+  }
 
   ngOnInit(): void {
     this.store.dispatch(fetchBoard());
   }
 
   handleTileClick(tile: string, position: number) {
-    let isValidMoveResult = false;
-    this.currentPlayerPosition.pipe(first()).subscribe((value) => {
-      if (value !== null) {
-        isValidMoveResult = isValidMove(value, position);
+    if (this.currentPlayerPosition) {
+      if (!isValidMove(this.currentPlayerPosition, position)) {
+        return;
       }
-    });
-    if (!isValidMoveResult) {
-      return;
     }
     if (['P', 'S', 'E'].includes(tile)) {
       this.store.dispatch(makeMove({ position }));
-    } else if (tile === 'M') {
-      this.player
-        .pipe(
-          map((player) => {
-            if (player !== null) {
-              this.store.dispatch(
-                startFight({ playerId: player.id, monsterId: 1 })
-              );
-            }
-          })
-        )
-        .subscribe();
+    } else if (tile === 'M' && this.player) {
+      this.store.dispatch(
+        startFight({ playerId: this.player.id, monsterId: 1 })
+      );
     }
   }
 }
