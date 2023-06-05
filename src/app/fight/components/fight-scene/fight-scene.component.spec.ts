35301b5ec611d68df17b9411c7504e741aa698aa
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 16:10:28 -0500

    feat(fight): create fight-scene component and unit tests

--- src/app/fight/components/fight-scene/fight-scene.component.spec.ts
+++ src/app/fight/components/fight-scene/fight-scene.component.spec.ts
@@ -1,21 +1,48 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FightSceneComponent } from './fight-scene.component';
+import { Store, StoreModule } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { playerPerformAction } from '../../state/fight.actions';
+import { ActionType } from '../../fight.model';
+import { FightModule } from '../../fight.module';
 
 describe('FightSceneComponent', () => {
   let component: FightSceneComponent;
   let fixture: ComponentFixture<FightSceneComponent>;
+  let store: MockStore;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [StoreModule.forRoot({}), FightModule],
+      providers: [provideMockStore()],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [FightSceneComponent]
-    });
     fixture = TestBed.createComponent(FightSceneComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    store = TestBed.inject(Store) as MockStore;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch playerPerformAction', () => {
+    spyOn(store, 'dispatch').and.callThrough();
+    const actionType = ActionType.ATTACK;
+    component.handlePerformAction(actionType);
+    expect(store.dispatch).toHaveBeenCalledWith(
+      playerPerformAction({ actionType })
+    );
+  });
+
+  // it('should set opponentTurn to true', () => {
+  //   component.handlePerformAction(ActionType.ATTACK);
+  //   expect(component.opponentTurn).toBeTrue();
+  // });
+
+  afterEach(() => {
+    fixture.destroy();
   });
 });
--- src/app/fight/components/fight-scene/fight-scene.component.ts
+++ src/app/fight/components/fight-scene/fight-scene.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActionType } from '../../fight.model';
+import { Store } from '@ngrx/store';
+import { playerPerformAction } from '../../state/fight.actions';
+import {
+  selectFightOpponent,
+  selectOpponentTurn,
+} from '../../state/fight.selectors';
+import { log } from 'src/app/utils/log';
 
 @Component({
   selector: 'app-fight-scene',
   templateUrl: './fight-scene.component.html',
-  styleUrls: ['./fight-scene.component.scss']
+  styleUrls: ['./fight-scene.component.scss'],
 })
-export class FightSceneComponent {
+export class FightSceneComponent implements OnDestroy {
+  actionType = ActionType;
+  opponent$ = this.store.select(selectFightOpponent);
+  opponentTurn$ = this.store.select(selectOpponentTurn);
+  constructor(private store: Store) {}
 
+  ngOnDestroy(): void {
+    log('fight scene destroy');
+  }
+
+  handlePerformAction(actionType: ActionType) {
+    this.store.dispatch(playerPerformAction({ actionType }));
+  }
 }
