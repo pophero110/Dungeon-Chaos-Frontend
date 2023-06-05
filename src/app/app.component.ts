9b86bad310e11b4af3be1122f52b1ac043f35f8e
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 12:17:27 -0500

    refactor(app): app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,8 @@
 import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { selectPlayer } from './player/state/player.selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'dungeon-chaos-frontend';
-
-  player$ = this.store.select(selectPlayer);
-  constructor(private store: Store) {}
-}
+export class AppComponent {}
