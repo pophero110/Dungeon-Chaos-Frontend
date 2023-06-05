5c699e30bd4abebbc823300519a84bbb2f0762ef
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 16:10:59 -0500

    refactor(fight): fight related data type

--- src/app/fight/fight.model.ts
+++ src/app/fight/fight.model.ts
@@ -1,16 +1,26 @@
+import { Player } from '../player/player.model';
+
 export interface Fight {
   id: number;
   fightResult: FightResult;
   opponent: Opponent;
+  player: Player;
+  currentTurn: CurrentTurn;
 }
 
 export enum FightResult {
-  VICTORY_PLAYER = 'Victory - Player',
-  VICTORY_MONSTER = 'Victory - Monster',
-  FLEE = 'Flee',
+  VICTORY_PLAYER = 'VICTORY_PLAYER',
+  VICTORY_OPPONENT = 'VICTORY_OPPONENT',
+  FLEE = 'FLEE',
+  ONGOING = 'ONGOING',
 }
 
-interface Opponent {
+export enum CurrentTurn {
+  PLAYER = 'PLAYER',
+  OPPONENT = 'OPPONENT',
+}
+
+export interface Opponent {
   id: number;
   name: string;
   health: number;
@@ -18,3 +28,11 @@ interface Opponent {
   defense: number;
   speed: number;
 }
+
+export enum ActionType {
+  ATTACK = 'ATTACK',
+  DEFEND = 'DEFEND',
+  FLEE = 'FLEE',
+}
+
+export const OPPONENT_TURN_DURATION = 1000; // 1 second
