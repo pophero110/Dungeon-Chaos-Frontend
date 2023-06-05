01340d786ca3d17921f1ce8cef9fe2456ed0df74
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 16:11:21 -0500

    feat(fight): include playerModule in fightModule

--- src/app/fight/fight.module.ts
+++ src/app/fight/fight.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TokenModule } from '../token/token.module';
 import { FightSceneComponent } from './components/fight-scene/fight-scene.component';
+import { PlayerModule } from '../player/player.module';
 
 @NgModule({
   declarations: [FightSceneComponent],
-  imports: [CommonModule, TokenModule],
+  imports: [CommonModule, TokenModule, PlayerModule],
   exports: [FightSceneComponent],
 })
 export class FightModule {}
