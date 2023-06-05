e1b328c2642ea5a68634ecb2a249873bdc006e17
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-04 21:33:10 -0500

    feat(fight): add new fight api
    1. playerPerformAction
    2. opponentPerformAction
    3. playerWinFight

--- src/app/utils/api/fightApi.ts
+++ src/app/utils/api/fightApi.ts
@@ -7,6 +7,18 @@ export interface FightRequest {
 
 export const startFightUlr = `${environment.apiUrl}/api/fights/start`;
 
-export const playerPerformActionUrl = (fightId: number): string => {
+export const performActionUrl = (fightId: number): string => {
   return `${environment.apiUrl}/api/fights/${fightId}/performAction`;
+};
+
+export const playerPerformActionUrl = (fightId: number): string => {
+  return `${environment.apiUrl}/api/fights/${fightId}/playerPerformAction`;
+};
+
+export const opponentPerformActionUrl = (fightId: number): string => {
+  return `${environment.apiUrl}/api/fights/${fightId}/opponentPerformAction`;
+};
+
+export const playerWinFightUrl = (fightId: number): string => {
+  return `${environment.apiUrl}/api/fights/${fightId}/playerWinFight`;
 };
