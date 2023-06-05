051a02bd40e6697d98d71c66dbe6b4b637769760
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 12:44:14 -0500

    feat(character): add unit tests for character container component

--- src/app/character/components/character-container/character-container.component.ts
+++ src/app/character/components/character-container/character-container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { createPlayer } from 'src/app/player/state/player.actions';
 import { selectIsPlayerCreated } from 'src/app/player/state/player.selectors';
@@ -7,23 +7,27 @@ import {
   selectCharacters,
   selectSelectedCharacter,
 } from '../../state/character.selectors';
+import { log } from 'src/app/utils/log';
 
 @Component({
   selector: 'app-character-container',
   templateUrl: './character-container.component.html',
   styleUrls: ['./character-container.component.scss'],
 })
-export class CharacterContainerComponent {
+export class CharacterContainerComponent implements OnInit {
   selectedCharacterId!: number;
   characters$ = this.store.select(selectCharacters);
   selectedCharacter$ = this.store.select(selectSelectedCharacter);
   isPlayerCreated$ = this.store.select(selectIsPlayerCreated);
+
   constructor(private store: Store) {}
+
   ngOnInit(): void {
     this.store.dispatch(fetchCharacter());
   }
 
   onCharacterSelection() {
+    log('select characte, id: ' + this.selectedCharacterId);
     this.store.dispatch(
       selectCharacter({ selectedCharacterId: this.selectedCharacterId })
     );
