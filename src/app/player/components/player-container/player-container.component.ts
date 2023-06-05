31298ab08d18928dba055c6d29c86cb63a042b4d
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 17:09:53 -0500

    feat(player): create play-container component and unit tests

---
+++ src/app/player/components/player-container/player-container.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import {
+  selectPlayerState,
+  selectIsPlayerCreated,
+} from '../../state/player.selectors';
+
+@Component({
+  selector: 'app-player-container',
+  templateUrl: './player-container.component.html',
+  styleUrls: ['./player-container.component.scss'],
+})
+export class PlayerContainerComponent {
+  player$ = this.store.select(selectPlayerState);
+  isPlayerCreated$ = this.store.select(selectIsPlayerCreated);
+  constructor(private store: Store) {
+    this.isPlayerCreated$.subscribe((isPlayerCreated) => {
+      console.log({ isPlayerCreated });
+    });
+  }
+}
