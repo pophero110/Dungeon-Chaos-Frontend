05724b1b4cab68a2082769fa1f7fa44817f669b7
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 12:44:42 -0500

    test(character): fix fail test

--- src/app/character/service/character.service.spec.ts
+++ src/app/character/service/character.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CharacterService } from './character.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CharacterService', () => {
   let service: CharacterService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(CharacterService);
   });
 
