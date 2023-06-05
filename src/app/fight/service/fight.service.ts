1a127579901b599c0dfbb2e09ffed9e2d4cb0f01
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 16:10:00 -0500

    feat(fight): add fight service and unit tests

--- src/app/fight/service/fight.service.ts
+++ src/app/fight/service/fight.service.ts
@@ -1,6 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { FightRequest, startFightUlr } from 'src/app/utils/api/fightApi';
+import {
+  FightRequest,
+  opponentPerformActionUrl,
+  playerWinFightUrl,
+  startFightUlr,
+} from 'src/app/utils/api/fightApi';
+import { playerPerformActionUrl } from 'src/app/utils/api/fightApi';
+import { ActionType } from '../fight.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +16,17 @@ export class FightService {
   constructor(private http: HttpClient) {}
   public startFight(fightRequest: FightRequest) {
     return this.http.post(startFightUlr, fightRequest);
+  }
+
+  public playerPerformAction(fightId: number, actionType: ActionType) {
+    return this.http.put(playerPerformActionUrl(fightId), { actionType });
+  }
+
+  public opponentPerformAction(fightId: number) {
+    return this.http.put(opponentPerformActionUrl(fightId), {});
+  }
+
+  public playerWinFight(fightId: number) {
+    return this.http.post(playerWinFightUrl(fightId), {});
   }
 }
