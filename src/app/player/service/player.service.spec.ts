aaac58e2febb1f4a8c7a412ec51926627dc844d4
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 09:48:43 -0500

    feat(player): test player service

--- src/app/player/service/player.service.spec.ts
+++ src/app/player/service/player.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PlayerService } from './player.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { createPlayerUrl } from 'src/app/utils/api/playerApi';
+import { fakePlayerState } from 'src/app/test/fakeState';
 
 describe('PlayerService', () => {
   let service: PlayerService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PlayerService],
+    });
     service = TestBed.inject(PlayerService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should send a POST request to create player', () => {
+    const selectedCharacterId = '123';
+    const dummyResponse = fakePlayerState;
+
+    service.createPlayer(selectedCharacterId).subscribe((response) => {
+      expect(response).toEqual(dummyResponse);
+    });
+
+    const request = httpMock.expectOne(createPlayerUrl);
+    expect(request.request.method).toBe('POST');
+    expect(request.request.body).toEqual({ selectedCharacterId });
+
+    request.flush(dummyResponse);
   });
 });
