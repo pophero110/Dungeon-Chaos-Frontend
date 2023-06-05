8282af7c0f4b8af3989317eaa8547af648db950a
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 10:46:45 -0500

    feat(board): add tests to tile component

--- src/app/board/components/tile/tile.component.spec.ts
+++ src/app/board/components/tile/tile.component.spec.ts
@@ -1,21 +1,73 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { StoreModule } from '@ngrx/store';
 import { TileComponent } from './tile.component';
+import { By } from '@angular/platform-browser';
+import { BoardModule } from '../../board.module';
 
 describe('TileComponent', () => {
   let component: TileComponent;
   let fixture: ComponentFixture<TileComponent>;
+  let mockHandleTileClick: jasmine.Spy;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BoardModule, StoreModule.forRoot({})],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [TileComponent]
-    });
     fixture = TestBed.createComponent(TileComponent);
     component = fixture.componentInstance;
+    mockHandleTileClick = jasmine.createSpy('handleTileClick');
+    component.handleTileClick = mockHandleTileClick;
+    component.tileType = 'P';
+    component.position = 1;
+    component.currentPlayerPosition = 2;
+    component.playerCharacterName = 'Warrior';
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call handleTileClick when the tile is clicked', () => {
+    const tileElement: HTMLElement = fixture.nativeElement.querySelector('div');
+    tileElement.click();
+    expect(mockHandleTileClick).toHaveBeenCalledWith('P', 1);
+  });
+
+  it('should display the warrior token when the player character name is "Warrior"', () => {
+    component.position = 2;
+    fixture.detectChanges();
+    const warriorTokenElement = fixture.debugElement.query(
+      By.css('app-warrior-token')
+    );
+    expect(warriorTokenElement).toBeTruthy();
+  });
+
+  it('should display the warrior token when the player character name is "Rogue"', () => {
+    component.playerCharacterName = 'Rogue';
+    fixture.detectChanges();
+    const rogueTokenElement = fixture.debugElement.query(
+      By.css('app-rogue-token')
+    );
+    expect(rogueTokenElement).toBeTruthy();
+  });
+
+  it('should display only one the warrior token representing player', () => {
+    component.position = 2;
+    fixture.detectChanges();
+    const warriorTokenElements = fixture.debugElement.queryAll(
+      By.css('app-warrior-token')
+    );
+    expect(warriorTokenElements.length).toBe(1);
+  });
+
+  it('should display the path token when the tile type is "P"', () => {
+    const pathTokenElement = fixture.debugElement.query(
+      By.css('app-path-token')
+    );
+    expect(pathTokenElement).toBeTruthy();
   });
 });
--- src/app/board/components/tile/tile.component.ts
+++ src/app/board/components/tile/tile.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-tile',
@@ -6,8 +7,11 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./tile.component.scss'],
 })
 export class TileComponent {
-  @Input() type!: string;
+  @Input() tileType!: string;
   @Input() position!: number;
   @Input() currentPlayerPosition!: number | null;
   @Input() playerCharacterName!: string | null | undefined;
+  @Input() handleTileClick!: (tileType: string, position: number) => void;
+
+  constructor(private store: Store) {}
 }
