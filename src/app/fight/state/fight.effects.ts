67be7fe49599d3e2115b009673caa39c86339a7e
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-05 16:08:55 -0500

    feat(fight): add fight effects and unit tests

--- src/app/fight/state/fight.effects.ts
+++ src/app/fight/state/fight.effects.ts
@@ -2,29 +2,150 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { FightService } from '../service/fight.service';
 import {
+  playerPerformActionError,
+  playerPerformActionSuccess,
+  playerPerformAction,
   startFight,
   startFightError,
   startFightSuccess,
+  opponentPerformAction,
+  opponentPerformActionSuccess,
+  playerLoseFight,
+  endFight,
 } from './fight.actions';
-import { catchError, map, mergeMap, of } from 'rxjs';
-import { Fight } from '../fight.model';
+import { catchError, concat, delay, mergeMap, of } from 'rxjs';
+import {
+  CurrentTurn,
+  FightResult,
+  OPPONENT_TURN_DURATION,
+} from '../fight.model';
+import { playerDie, updatePlayer } from 'src/app/player/state/player.actions';
+import {
+  fetchBoard,
+  removeMonsterToken,
+} from 'src/app/board/state/board.actions';
+import { opponentPerformActionError, playerWinFight } from './fight.actions';
+import { log, logStart } from 'src/app/utils/log';
+import { FightState } from './fight.reducer';
+import { PlayerState } from 'src/app/player/state/player.reducer';
 
 @Injectable()
 export class FightEffects {
+  fightId!: number;
+  opponentPosition!: number;
   startFight$ = createEffect(() =>
     this.actions$.pipe(
       ofType(startFight),
-      mergeMap(({ playerId, monsterId }) =>
+      mergeMap(({ monsterId, opponentPosition, playerId }) =>
         this.fightService.startFight({ playerId, monsterId }).pipe(
-          map((response) => {
-            console.log({ response });
-            return startFightSuccess({
-              fight: response as Fight,
+          mergeMap((response) => {
+            const fightState = response as FightState;
+            logStart('start fight', response);
+            this.opponentPosition = opponentPosition;
+            this.fightId = fightState.id;
+            const successAction = startFightSuccess({
+              fightState: { ...fightState },
             });
+            if (fightState.currentTurn === CurrentTurn.OPPONENT) {
+              return concat(of(successAction), of(opponentPerformAction()));
+            }
+            return concat(of(successAction));
           }),
-          catchError(() => of(startFightError))
+          catchError(() => of(startFightError()))
         )
       )
+    )
+  );
+
+  opponentPerformAction$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(opponentPerformAction),
+      mergeMap(() =>
+        this.fightService.opponentPerformAction(this.fightId).pipe(
+          mergeMap((response) => {
+            log('opponent perform action');
+            const fightState = response as FightState;
+            if (fightState.fightResult === FightResult.VICTORY_OPPONENT) {
+              return concat(of(playerLoseFight()));
+            } else {
+              const successAction = opponentPerformActionSuccess({
+                fightState: { ...fightState },
+              });
+              return concat(
+                of(
+                  updatePlayer({
+                    playerState: fightState.player as PlayerState,
+                  })
+                ).pipe(delay(OPPONENT_TURN_DURATION)),
+                of(successAction).pipe(delay(OPPONENT_TURN_DURATION))
+              );
+            }
+          }),
+          catchError(() => of(opponentPerformActionError()))
+        )
+      )
+    )
+  );
+
+  playerPerformAction$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(playerPerformAction),
+      mergeMap(({ actionType }) =>
+        this.fightService.playerPerformAction(this.fightId, actionType).pipe(
+          mergeMap((response) => {
+            log('player perform action');
+            const fightState = response as FightState;
+            if (fightState.fightResult === FightResult.VICTORY_PLAYER) {
+              return concat(of(playerWinFight()));
+            } else {
+              const successAction = playerPerformActionSuccess({
+                fightState: { ...fightState },
+              });
+              return concat(of(successAction), of(opponentPerformAction()));
+            }
+          }),
+          catchError(() => of(playerPerformActionError()))
+        )
+      )
+    )
+  );
+
+  playerWinFight$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(playerWinFight),
+      mergeMap(() =>
+        this.fightService.playerWinFight(this.fightId).pipe(
+          mergeMap((response) => {
+            log('player win fight');
+            const playerState = (response as { player: PlayerState }).player;
+            return concat(
+              of(endFight()),
+              of(
+                removeMonsterToken({
+                  position: this.opponentPosition,
+                })
+              ),
+              of(
+                updatePlayer({
+                  playerState: playerState,
+                })
+              )
+            );
+          }),
+          catchError(() => of(playerPerformActionError()))
+        )
+      )
+    )
+  );
+
+  playerLoseFight$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(playerLoseFight),
+      mergeMap(() => {
+        log('player lose fight');
+        return concat(of(endFight()), of(playerDie()), of(fetchBoard()));
+      }),
+      catchError(() => of(playerPerformActionError()))
     )
   );
 
