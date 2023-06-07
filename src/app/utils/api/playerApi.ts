a282972531403002fe710b90e84e8c2f82c76bb3
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-07 09:00:46 -0500

    feat(player): add getPlayer api for testing

--- src/app/utils/api/playerApi.ts
+++ src/app/utils/api/playerApi.ts
@@ -1,3 +1,5 @@
 import { environment } from 'src/environment';
 
 export const createPlayerUrl = `${environment.apiUrl}/api/players`;
+
+export const getPlayerUrl = `${environment.apiUrl}/api/players/1`;
