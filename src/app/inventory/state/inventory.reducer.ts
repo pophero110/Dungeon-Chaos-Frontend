fc9af4a85940ae93287ea58a7d7341838c94bf54
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-07 11:06:13 -0500

    feat(inventory): create inventory reducer, effect, action and selectors

---
+++ src/app/inventory/state/inventory.reducer.ts
@@ -0,0 +1,137 @@
+import { createReducer, on } from '@ngrx/store';
+import { InventoryItem } from '../inventory.model';
+import {
+  addItemToInventoryError,
+  addItemToInventorySuccess,
+  equipArmorError,
+  equipArmorSuccess,
+  equipWeaponError,
+  equipWeaponSuccess,
+  fetchInventoryError,
+  fetchInventorySuccess,
+  hideItemPanel,
+  initializeInventory,
+  removeItemFromInventoryError,
+  removeItemFromInventorySuccess,
+  showItemPanel,
+  unequipArmorError,
+  unequipArmorSuccess,
+  unequipWeaponError,
+  unequipWeaponSuccess,
+} from './inventory.actions';
+import { log } from 'src/app/utils/log';
+
+export interface InventoryState {
+  id: number;
+  inventoryItems: InventoryItem[];
+  selectedInventoryItem: InventoryItem;
+}
+
+const initialState: InventoryState | object = {};
+
+export const inventoryReducer = createReducer(
+  initialState,
+  on(initializeInventory, (state, inventoryState) => {
+    log('initialize inventory', inventoryState);
+    return {
+      ...state,
+      ...inventoryState,
+    };
+  }),
+  on(showItemPanel, (state, { inventoryItem }) => {
+    log('show item panel', { inventoryItem });
+    return {
+      ...state,
+      selectedInventoryItem: inventoryItem,
+    };
+  }),
+  on(hideItemPanel, (state) => {
+    log('hide item panel');
+    return {
+      ...state,
+      selectedInventoryItem: null,
+    };
+  }),
+  on(equipWeaponSuccess, (state, { inventoryState }) => {
+    log('equip weapon success', inventoryState);
+    return {
+      ...state,
+      ...inventoryState,
+    };
+  }),
+  on(equipWeaponError, (state, { error }) => {
+    log('equip weapon error', error);
+    return {
+      ...state,
+    };
+  }),
+  on(unequipWeaponSuccess, (state, { inventoryState }) => {
+    log('unequip weapon success', inventoryState);
+    return {
+      ...state,
+      ...inventoryState,
+    };
+  }),
+  on(unequipWeaponError, (state, { error }) => {
+    log('unequip weapon error', error);
+    return {
+      ...state,
+    };
+  }),
+  on(equipArmorSuccess, (state, { inventoryState }) => {
+    log('equip armor success', inventoryState);
+    return {
+      ...state,
+      ...inventoryState,
+    };
+  }),
+  on(equipArmorError, (state, { error }) => {
+    log('equip armor error', error);
+    return {
+      ...state,
+    };
+  }),
+  on(unequipArmorSuccess, (state, { inventoryState }) => {
+    log('unequip armor success', inventoryState);
+    return {
+      ...state,
+      ...inventoryState,
+    };
+  }),
+  on(unequipArmorError, (state, { error }) => {
+    log('unequip armor error', error);
+    return {
+      ...state,
+    };
+  }),
+  on(fetchInventorySuccess, (state, { inventoryState }) => {
+    return {
+      ...state,
+      ...inventoryState,
+    };
+  }),
+  on(fetchInventoryError, (state, { error }) => {
+    console.error('fetch inventory error', error);
+    return { ...state };
+  }),
+  on(addItemToInventorySuccess, (state, { inventoryState }) => {
+    return {
+      ...state,
+      ...inventoryState,
+    };
+  }),
+  on(addItemToInventoryError, (state, { error }) => {
+    console.error('add item to inventory error', error);
+    return { ...state };
+  }),
+  on(removeItemFromInventorySuccess, (state, { inventoryState }) => {
+    return {
+      ...state,
+      ...inventoryState,
+    };
+  }),
+  on(removeItemFromInventoryError, (state, { error }) => {
+    console.error('remove item to inventory error', error);
+    return { ...state };
+  })
+);
