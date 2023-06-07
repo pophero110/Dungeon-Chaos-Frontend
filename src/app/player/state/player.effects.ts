ee58ec9a9bbd3680981270a01a7f8cdd1dc90df1
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-07 09:38:11 -0500

    feat(player): setup inventory after player creation

--- src/app/player/state/player.effects.ts
+++ src/app/player/state/player.effects.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
-import { catchError, map, mergeMap } from 'rxjs/operators';
+import { concat, of } from 'rxjs';
+import { catchError, mergeMap } from 'rxjs/operators';
 import {
   createPlayer,
   createPlayerError,
@@ -9,6 +9,9 @@ import {
 } from './player.actions';
 import { PlayerService } from '../service/player.service';
 import { PlayerState } from './player.reducer';
+import { initializeInventory } from 'src/app/inventory/state/inventory.actions';
+import { InventoryState } from 'src/app/inventory/state/inventory.reducer';
+import { log } from 'src/app/utils/log';
 
 @Injectable()
 export class PlayerEffects {
@@ -17,10 +20,20 @@ export class PlayerEffects {
       ofType(createPlayer),
       mergeMap(({ selectedCharacterId }) => {
         return this.playerService.createPlayer(selectedCharacterId).pipe(
-          map((response) => {
-            return createPlayerSuccess({
-              playerState: response as PlayerState,
-            });
+          mergeMap((response) => {
+            log('create player', { selectedCharacterId });
+            return concat(
+              of(
+                createPlayerSuccess({
+                  playerState: response as PlayerState,
+                })
+              ),
+              of(
+                initializeInventory({
+                  ...(response as { inventory: InventoryState }).inventory,
+                })
+              )
+            );
           }),
           catchError(() => of(createPlayerError()))
         );
