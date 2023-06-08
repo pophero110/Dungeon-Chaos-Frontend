912cf0047fc30e01820f0ce4e505318326753c31
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-08 08:44:54 -0500

    feat(board): import utils function in reducer

--- src/app/board/state/board.reducer.ts
+++ src/app/board/state/board.reducer.ts
@@ -5,7 +5,12 @@ import {
   makeMove,
   removeToken,
 } from './board.actions';
-import { convertToMatrixIndex } from '../board.utils';
+import {
+  convertArrayIndexToMatrixIndex,
+  uncoverSurroundingTiles,
+  isVisitedTile,
+} from '../board.utils';
+import { log } from 'src/app/utils/log';
 
 export interface BoardState {
   tileMatrix: string[][];
@@ -21,27 +26,37 @@ export const initialState: BoardState = {
 
 export const boardReducer = createReducer(
   initialState,
-  on(fetchBoardSuccess, (state, { tileMatrix, currentPlayerPosition }) => ({
-    ...state,
-    tileMatrix,
-    currentPlayerPosition,
-  })),
+  on(fetchBoardSuccess, (state, { tileMatrix, currentPlayerPosition }) => {
+    log('fetch board success', tileMatrix);
+    return {
+      ...state,
+      tileMatrix,
+      currentPlayerPosition,
+    };
+  }),
   on(fetchBoardError, (state) => {
     throw new Error('Fetch Board Error');
     return { ...state };
   }),
   on(makeMove, (state, { position }) => {
+    if (isVisitedTile({ tileMatrix: state.tileMatrix, position })) {
+      const tileMatrix = uncoverSurroundingTiles(state.tileMatrix, position);
+      return {
+        ...state,
+        tileMatrix,
+        currentPlayerPosition: position,
+      };
+    }
     return {
       ...state,
-      currentPlayerPosition: position,
     };
   }),
   on(removeToken, (state, { position }) => {
     if (position) {
-      const { row, col } = convertToMatrixIndex(position);
+      const { row, col } = convertArrayIndexToMatrixIndex(position);
       const tileMatrix = [...state.tileMatrix];
       tileMatrix[row] = [...tileMatrix[row]];
-      tileMatrix[row][col] = 'P';
+      tileMatrix[row][col] = 'P!';
       return {
         ...state,
         tileMatrix: [...tileMatrix],
