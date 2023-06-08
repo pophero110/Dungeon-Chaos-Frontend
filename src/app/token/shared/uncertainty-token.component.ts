e26a00d076ac52e314758331c748b86dd453241f
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-08 16:03:21 -0500

    feat(token): adding uncertainty token

---
+++ src/app/token/shared/uncertainty-token.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uncertainty-token',
+  templateUrl: './uncertainty-token.component.html',
+})
+export class UncertaintyTokenComponent {}
