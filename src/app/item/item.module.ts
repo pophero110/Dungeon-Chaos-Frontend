d663de98c996e704db2a7ee82d498a07a7c6623e
Jiajin Ou <jiajin.ou@chowbus.com>
2023-06-07 20:27:41 -0500

    feat(item): create item module and item container component

---
+++ src/app/item/item.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ItemContainerComponent } from './components/item-container/item-container.component';
+
+@NgModule({
+  declarations: [ItemContainerComponent],
+  exports: [ItemContainerComponent],
+})
+export class ItemModule {}
